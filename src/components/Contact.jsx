2b1032ec9eee40b91271703c37b91d2788068788
mperorjohn <mperorjohnoyekola@gmail.com>
2025-04-15 22:48:57 +0100

    concluded component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import {  Container } from 'react-bootstrap';
+import Avatar from './Avatar';
+// import { Alert } from 'bootstrap';
+import Button from './Button';
 
-const Contact = ({name, email, phoneNumber, gitHub, linkedIn}) => {
+const Contact = ({name, email, phoneNumber, gitHub, linkedIn, image, alt, buttonName}) => {
   return (
     <div className='container d-flex justify-content-center' >
           <div className='row'>
               <div className=' bg-light p-5'>
+                 <Avatar image={image} alt={alt}/>
                   <h2>Contact Me</h2>
                   <h3>{name}</h3>
                   <p>{email}</p>
@@ -14,6 +18,7 @@ const Contact = ({name, email, phoneNumber, gitHub, linkedIn}) => {
                   <Link to={gitHub}>Github</Link><br/>
                   <Link to={email}>Email</Link><br/>
                   <Link to={linkedIn}>LinkedIn</Link>
+                  <Button name={buttonName}/>
                 </div>
           </div>
     </div>
